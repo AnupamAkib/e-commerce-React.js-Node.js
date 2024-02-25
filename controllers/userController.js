2f1fcde4d29590e3d89d0bb892c7f9f52d4a89a8
Mir Anupam Hossain Akib <mirakib25@gmail.com>
2024-02-26 01:20:44 +0600

    Input validation added in mongoose schema

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,11 +5,18 @@ const createUser = async(req, res) => {
         const _user = await User.create(req.body);
         console.log("user successfully created!");
         console.log(req.body);
-        res.status(200).json(_user);
+        res.status(200).json({message : "success", user : _user});
     }
     catch(error){
-        console.log(error.message);
-        res.status(500).json({message : error.message});
+        if (error.name === 'ValidationError') {
+            // Handle validation errors
+            const errors = Object.values(error.errors).map(err => err.message);
+            return res.status(400).json({ message : errors[0], errors }); //only send first error at once
+        } else {
+            // Handle other errors
+            console.error(error);
+            res.status(500).send({message : "Server Error"});
+        }
     }
 };
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,34 +1,165 @@
 const mongoose = require("mongoose");
+const md5 = require("md5");
 
 const userSchema = mongoose.Schema(
     {
         firstName : {
             type : String,
-            required : true
+            required : true,
+            trim : true,
+            validate : [
+                {
+                    validator: function(v) {
+                        // Custom validator function to check if the string contains only letters
+                        return /^[A-Za-z]+$/.test(v);
+                    },
+                    message: "First name must contain only letters." //it will fire when validator function return false
+                },
+                {
+                    validator: function(v) {
+                        if(v.length > 25) return false;
+                    },
+                    message: props => `The first name is too large.`
+                }
+            ]
         },
         lastName : {
             type : String,
-            required : true
+            required : true,
+            trim : true,
+            validate : [
+                {
+                    validator: function(v) {
+                        // Custom validator function to check if the string contains only letters
+                        return /^[A-Za-z]+$/.test(v);
+                    },
+                    message: "Last name must contain only letters."
+                },
+                {
+                    validator: function(v) {
+                        if(v.length > 25) return false;
+                    },
+                    message: props => `The last name is too large.`
+                }
+            ]
         },
         username : {
             type : String,
-            required : true
+            unique : true,
+            required : true,
+            trim : true,
+            //minlength: 6,
+            //maxlength: 25,
+            validate: [
+                {
+                    validator: async function(v) { //check if already exist
+                        const existingUser = await this.constructor.findOne({ username: v });
+                        return !existingUser;
+                    },
+                    message: props => `The username is already in use. Please choose a different one.`
+                },
+                {
+                    validator: function(v) { //check length of username
+                        if(v.length < 6 || v.length > 20){
+                            return false;
+                        }
+                    },
+                    message: props => `The username must contain 6 to 20 characters.`
+                },
+                {
+                    validator: function(v) { // Custom validator function to check if the string contains only numbers
+                        for(let i=0; i<v.length; i++){
+                            if((v[i]>='a' && v[i]<='z') || (v[i]>='A' && v[i]<='Z')) return true;
+                        }
+                        return false;
+                    },
+                    message: "Username must not contain only numbers."
+                }
+            ]
         },
         password : {
             type : String,
-            required : true
+            required : true,
+            validate : [
+                {
+                    validator: function(v) { //check length of username
+                        if(v.length < 6 || v.length > 20){
+                            return false;
+                        }
+                    },
+                    message: props => `The password must contain 6 to 20 characters.`
+                }
+            ]
+        },
+        profilePicture : {
+            type : mongoose.Schema.Types.Mixed, //json file
+            default : {
+                URL : "#",
+                name : ""
+            }
         },
         phoneNumber : {
             type : String,
-            required : true
+            required : true,
+            trim : true,
+            validate : [
+                {
+                    validator: function(v) {
+                        if(v.length != 11) return false;
+                        if(!(v[0]=='0' && v[1]=='1')) return false;
+                        for(let i=0; i<v.length; i++){
+                            if(!(v[i]>='0' && v[i]<='9')){
+                                return false;
+                            }
+                        }
+                    },
+                    message: props => `Invalid Phone Number.`
+                }
+            ]
         },
         emailAddress : {
             type : String,
-            required : true
+            unique : true,
+            required : true,
+            trim : true,
+            validate: [
+                {
+                    validator: async function(v) {
+                        const existingEmail = await this.constructor.findOne({ emailAddress: v });
+                        return !existingEmail;
+                    },
+                    message: props => `The email is already in use. Please choose a different one.`
+                },
+                {
+                    validator: function(v) {
+                        if(v.length > 50) return false;
+                    },
+                    message: props => `The email is too large.`
+                },
+                {
+                    validator: function(v) {
+                        // Regular expression for email validation
+                        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+                    },
+                    message: props => `Invalid email address.`
+                }
+            ]
         },
         shippingAddress : {
             type : String,
-            required : false
+            required : false,
+            validate : [
+                {
+                    validator: function(v) {
+                        if(v.length > 150) return false;
+                    },
+                    message: props => `The shipping address is too large.`
+                }
+            ]
+        },
+        wishList : {
+            type : [String], //array
+            default : [] //push product id
         }
     },
     {
@@ -36,5 +167,31 @@ const userSchema = mongoose.Schema(
     }
 );
 
+// Middleware to capitalize first character of firstName and lastName before saving and hash password
+userSchema.pre('save', function(next) {
+    this.firstName = capitalizeFirstLetter(this.firstName);
+    this.lastName = capitalizeFirstLetter(this.lastName);
+    this.password = md5(this.password);
+    next();
+});
+
+// Function to capitalize the first letter of each word
+function capitalizeFirstLetter(str) {
+    return str.replace(/\b\w/g, char => char.toUpperCase());
+}
+
+// Define a virtual property for fullName
+userSchema.virtual('fullName').get(function() {
+    return this.firstName.trim() + " " + this.lastName.trim();
+});
+
+//enable virual property 
+userSchema.set('toObject', { virtuals: true });
+userSchema.set('toJSON', { virtuals: true });
+//enable unique index for unique value in schema
+userSchema.index({ username: 1 }, { unique: true });
+
+
+//export model
 const User = mongoose.model("User", userSchema);
 module.exports = User;
